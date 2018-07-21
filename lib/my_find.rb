a7883ac7d0f4c23d5e438c0da330993d8c5f580a
Juan Tenezaca <tenj34@gmail.com>
2018-07-21 17:36:09 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,8 +1,12 @@
 require 'pry'
+# When it comes to doing a lower lever iterator we must have a counter
+
+
 
 def my_find(collection)
   i = 0
   while i < collection.length
+    yield(collection.length)
     i = i + 1
   end
 end
